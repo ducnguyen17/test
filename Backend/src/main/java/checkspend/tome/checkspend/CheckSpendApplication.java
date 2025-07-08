e649ed355d4c142a580fc44afbbd087b5488af16
Quagmire <nmduc1762001@gmail.com>
2025-07-08 21:45:09 +0700

    base backend

---
+++ Backend/src/main/java/checkspend/tome/checkspend/CheckSpendApplication.java
@@ -0,0 +1,13 @@
+package checkspend.tome.checkspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckSpendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckSpendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/checkspend/tome/checkspend/CheckSpendApplicationTests.java
@@ -0,0 +1,13 @@
+package checkspend.tome.checkspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckSpendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
